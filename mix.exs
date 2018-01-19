d6930e187b45c45a8651b01d6ce350112191e439
Michael Dorman <mdorman@signal.co>
2018-01-19 14:00:22 -0600

    version 0.2.6

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule SftpEx.Mixfile do
   def project do
     [
       app: :sftp_ex,
-      version: "0.2.5",
+      version: "0.2.6",
       elixir: ">= 1.3.0",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
